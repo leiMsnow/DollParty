b7f04a03803027625a5e63e8f14cfd61090b5507
ray <always_0122@163.com>
2017-05-02 21:35:48 +0800

    add

--- app/src/main/java/com/doll/party/app/ui/MainActivity.java
+++ app/src/main/java/com/doll/party/app/ui/MainActivity.java
@@ -1,10 +1,36 @@
 package com.doll.party.app.ui;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.TextView;
+
 import com.doll.party.app.R;
 import com.doll.party.app.base.BaseAppActivity;
 
 public class MainActivity extends BaseAppActivity {
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_main;
@@ -12,6 +38,8 @@ public class MainActivity extends BaseAppActivity {
 
     @Override
     protected void initData() {
-        hideToolbarBack();
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
